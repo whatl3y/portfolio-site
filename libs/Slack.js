1055ceea26ae6a248c8241bd571558072ee76190
Lance Whatley <whatl3y@gmail.com>
2019-08-11 12:42:59 -0400

    Update modules and executive summary

--- libs/Slack.js
+++ libs/Slack.js
@@ -6,16 +6,10 @@ const IncomingWebhook = slackClient.IncomingWebhook
 module.exports = {
   webhook: (!!config.slack.webhookUrl) ? new IncomingWebhook(config.slack.webhookUrl) : null,
 
-  send(content) {
-    return new Promise((resolve, reject) => {
-      if (!this.webhook)
-        return resolve(false)
+  async send(text) {
+    if (!this.webhook)
+      return resolve(false)
 
-      this.webhook.send(content, (err, header, statusCode, body) => {
-        if (err) return reject(err)
-        if (statusCode >= 400) return reject(body)
-        resolve(body)
-      })
-    })
+    return await this.webhook.send({ text })
   }
 }
